914b75ec034a6fbd509774e482241244d9ca9f69
Kevin Stares-Darbon <red.inshou@gmail.com>
2024-08-21 15:36:41 +0100

    adds an element to the display

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,12 @@
 displayElement = document.querySelector("#display");
+
+console.log(displayElement);
+
+let character = document.createElement("div");
+
+character.setAttribute("style", "position: absolute; bottom: 10px; left: 10px; height: 50px; width: 50px; background-color: blue");
+
+displayElement.appendChild(character);
+
+
+
