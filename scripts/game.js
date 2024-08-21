2e48b7d10da171cdd691b1b2e7b98d986526c223
Muhammed Almutasim Ibrahim <www.tota.11@gmail.com>
2024-08-21 21:06:39 +0300

    obestacle feature

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,5 @@
-displayElement = document.querySelector("#display");
+const obstecale = document.querySelector('obstecale');
+const character = document.querySelect('character');
+const gameContainer = documentqq.querySelect('game-container');
+
+
